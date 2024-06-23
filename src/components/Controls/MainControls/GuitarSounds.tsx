93977ad212b9f757e0f409fa1ee0ade0530b5cb3
jDelille <justind7513@gmail.com>
2024-06-22 22:38:11 -0700

    refactor: updated guitar sounds component

--- src/components/Controls/MainControls/GuitarSounds.tsx
+++ src/components/Controls/MainControls/GuitarSounds.tsx
@@ -1,35 +1,28 @@
+import React from 'react';
 import { observer } from 'mobx-react';
 import { GuitarConstants } from '../../../constants/@GuitarConstants';
 import Store from '../../../mobx/Store';
 import SoundModal from '../../Modals/SoundModal';
 
-export type GuitarSoundsProps = {
+type GuitarSoundsProps = {
   isGuitarSoundsModal: boolean;
   toggleGuitarSounds: () => void;
 };
 
-function formatString<T extends string>(str: T) {
-  const words = str.split('_');
-  const capitalizedWords = words.map((word) => {
-    const firstLetter = word.charAt(0).toUpperCase();
-    const restOfWord = word.slice(1);
-    return firstLetter + restOfWord;
-  });
-  const formattedString = capitalizedWords.join(' ');
-  return formattedString;
-}
+const GuitarSounds: React.FC<GuitarSoundsProps> = observer(({ isGuitarSoundsModal, toggleGuitarSounds }) => {
+  const activeSound = Store.sound;
+  const guitarNames: string[] = GuitarConstants.guitars;
 
-const GuitarSounds = observer(
-  ({ isGuitarSoundsModal, toggleGuitarSounds }: GuitarSoundsProps) => {
-    const activeSound = Store.sound;
-    const guitarNames: string[] = GuitarConstants.guitars;
-    return (
-      <button type="button" className="content" onClick={toggleGuitarSounds}>
-        <p>{formatString(activeSound)}</p>
-        {isGuitarSoundsModal && <SoundModal sounds={{ name: guitarNames }} />}
-      </button>
-    );
+  function formatString(str: string): string {
+    return str.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   }
-);
+
+  return (
+    <button type="button" className="content" onClick={toggleGuitarSounds}>
+      <p>{formatString(activeSound)}</p>
+      {isGuitarSoundsModal && <SoundModal sounds={{ name: guitarNames }} />}
+    </button>
+  );
+});
 
 export default GuitarSounds;
